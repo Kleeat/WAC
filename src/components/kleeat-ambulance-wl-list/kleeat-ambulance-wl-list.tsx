aacabbcc1f070a8e86025b5d9b024babfaf53eab
Levente Szénási <91260091+Kleeat@users.noreply.github.com>
2026-04-17 12:58:11 +0200

    Entry id fix

--- src/components/kleeat-ambulance-wl-list/kleeat-ambulance-wl-list.tsx
+++ src/components/kleeat-ambulance-wl-list/kleeat-ambulance-wl-list.tsx
@@ -45,7 +45,8 @@ export class KleeatAmbulanceWlList {
         ) : (
           <md-list>
             {this.waitingPatients.map((patient, index) => (
-              <md-list-item onClick={() => this.entryClicked.emit(index.toString())}>
+              // TODO: Fix ID
+              <md-list-item onClick={() => this.entryClicked.emit(patient.id)} key={patient.id}>
                 <div slot="headline">{patient.name}</div>
                 <div slot="supporting-text">{'Predpokladaný vstup: ' + patient.estimatedStart?.toLocaleString()}</div>
                 <md-icon slot="start">person</md-icon>
